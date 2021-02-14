034892f9d8802f7889eb036d31a126b6fe45751d
Artem Safin <artemsafin.webdev@gmail.com>
2021-02-15 01:35:54 +0300

    По маске

--- src/js/cvvMask.js
+++ src/js/cvvMask.js
@@ -4,7 +4,7 @@ import Inputmask from 'inputmask';
 export default function cvvMask() {
     const phoneInputs = Array.from(document.querySelectorAll('.js-card-cvv'));
     phoneInputs.forEach(input => {
-        const instance = new Inputmask({ mask: '999' });
+        const instance = new Inputmask({ mask: '999', showMaskOnFocus: false, showMaskOnHover: false, placeholder: "" });
         instance.mask(input);
     });
 }
