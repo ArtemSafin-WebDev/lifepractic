e6620cdc9430ebe1efb6087eb04e23125db44f43
Artem Safin <artemsafin.webdev@gmail.com>
2021-04-24 00:11:53 +0300

    Фиксы

--- src/js/modals.js
+++ src/js/modals.js
@@ -5,6 +5,8 @@ export default function() {
 
 
     function openModal(id) {
+        console.trace();
+        console.log('Opening modal', id)
         if (activeModal) {
             closeModal(activeModal);
         }
@@ -35,10 +37,14 @@ export default function() {
             const link = event.target.matches('.js-open-modal') ? event.target : event.target.closest('.js-open-modal');
             const hash = link.hash;
             openModal(hash);
+
+            console.log('Opening modal from 1 line', event.target, event.target.closest('.js-open-modal'))
         } else if (event.target.matches('.js-close-modal') || event.target.closest('.js-close-modal')) {
             event.preventDefault();
             const modalToClose = event.target.closest('.js-modal');
             closeModal(modalToClose);
+
+          
         } else if (event.target.matches('.js-modal')) {
             event.preventDefault();
             const modalToClose = event.target;
