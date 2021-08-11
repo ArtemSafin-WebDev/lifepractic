caf90be270eba5bbb7465e78e1e44186c676e970
Artem Safin <artemsafin.webdev@gmail.com>
2021-08-11 10:08:38 +0300

    Адаптив

--- src/js/reviewsSlider.js
+++ src/js/reviewsSlider.js
@@ -1,11 +1,11 @@
-import { Swiper, Navigation } from 'swiper';
+import { Swiper, Navigation, Pagination } from 'swiper';
 
-Swiper.use([Navigation]);
+Swiper.use([Navigation, Pagination]);
 
 export default function reviewsSlider() {
     const elements = Array.from(document.querySelectorAll('.js-reviews-slider'));
 
-    console.log('Reviews sliders', elements)
+    console.log('Reviews sliders', elements);
 
     elements.forEach(element => {
         const container = element.querySelector('.swiper-container');
@@ -13,12 +13,24 @@ export default function reviewsSlider() {
         new Swiper(container, {
             spaceBetween: 10,
             watchOverflow: true,
-            slidesPerView: 4,
-            slidesPerColumn: 2,
+            slidesPerView: 1,
+            slidesPerColumn: 1,
             slidesPerColumnFill: 'row',
             navigation: {
                 nextEl: element.querySelector('.reviews__slider-arrow--next'),
                 prevEl: element.querySelector('.reviews__slider-arrow--prev')
+            },
+            pagination: {
+                el: element.querySelector('.reviews__slider-pagination'),
+                type: 'bullets',
+                clickable: true
+            },
+            breakpoints: {
+                641: {
+                    slidesPerView: 4,
+                    slidesPerColumn: 2,
+                    slidesPerColumnFill: 'row'
+                }
             }
         });
     });
