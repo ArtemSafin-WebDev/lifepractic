a6bf95d6a8a8e01007568d1532ce5a92b12a8c5f
Albert <kamalov00albert@gmail.com>
2022-03-09 10:42:53 +0300

    Реализовал автоматический переход при заполнении номера карты

--- src/js/cardAutoswitch.js
+++ src/js/cardAutoswitch.js
@@ -2,24 +2,31 @@ export default function cardAutoswitch() {
     const elements = Array.from(document.querySelectorAll('.payment__cards'));
 
     elements.forEach(element => {
+        const number = element.querySelector('.js-card-number');
+        const name = element.querySelector('.js-card-name');
         const month = element.querySelector('.js-card-month');
         const year = element.querySelector('.js-card-year');
         const cvv = element.querySelector('.js-card-cvv');
 
         if (!month || !year || !cvv) return;
 
-        console.log('Autoswitch element')
+        number.addEventListener('input', event => {
+            let inputValue =  event.target.value;
+            inputValue = inputValue.replace(/_/gmi, '').trim();
+
+            if (inputValue.length >= 19) {
+                name.focus();
+            }
+        })
 
         month.addEventListener('input', event => {
             if (event.target.value.length >= 2) {
-                console.log('value', event.target.value)
                 year.focus();
             }
         })
 
         year.addEventListener('input', event => {
             if (event.target.value.length >= 2) {
-                console.log('value', event.target.value)
                 cvv.focus();
             }
         })
